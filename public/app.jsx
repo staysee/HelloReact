3a5d5c29bfc5d32d40c84fbab37538779fe7b079
Stacey Sugiono <staceysugiono@gmail.com>
2016-08-03 18:09:57 -0700

    nesting components, creating static components

--- public/app.jsx
+++ public/app.jsx
@@ -1,3 +1,25 @@
+var GreeterMessage = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>Some H1</h1>
+        <p>Some paragraph</p>
+      </div>
+    );
+  }
+});
+
+var GreeterForm = React.createClass({
+  render: function() {
+    return (
+      <form onSubmit={this.onButtonClick}>
+        <input type="text" ref="name"/>
+        <button>Set Name</button>
+      </form>
+    );
+  }
+});
+
 var Greeter = React.createClass({   //common create method
   getDefaultProps: function() {     //default prop, if the data wasn't provided
     return {
@@ -34,10 +56,15 @@ var Greeter = React.createClass({   //common create method
         <h1>Hello {name}!</h1>
         <p>{message + '!!'}</p>
 
+        <GreeterMessage/>
+
         <form onSubmit={this.onButtonClick}>
           <input type="text" ref="name"/>
           <button>Set Name</button>
         </form>
+        <GreeterForm/>
+
+
       </div>
     );
 
